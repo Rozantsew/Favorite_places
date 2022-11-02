fbffff575fd84fd0bda91939c1959f36187d13c6
Michał <mykhailorozantsev@gmail.com>
2022-11-02 18:06:32 +0100

    Initial Commit

---
+++ Favorite sity places/Favorite_sity_placesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorite_sity_placesApp.swift
+//  Favorite sity places
+//
+//  Created by Mykhailo Rozantsev on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Favorite_sity_placesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
